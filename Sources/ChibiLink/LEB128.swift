f2538619d7b88637f8306d6d5a62b8c7716355b6
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-01 17:15:53 +0900

    LEB128 encoder

--- Sources/ChibiLink/LEB128.swift
+++ Sources/ChibiLink/LEB128.swift
@@ -14,7 +14,7 @@ func decodeULEB128<T>(_ bytes: ArraySlice<UInt8>, _: T.Type) -> (value: T, offse
 }
 
 func decodeSLEB128<T>(_ bytes: ArraySlice<UInt8>, _: T.Type) -> (value: T, offset: Int)
-    where T: SignedInteger, T: FixedWidthInteger{
+    where T: SignedInteger, T: FixedWidthInteger {
     var index: Int = bytes.startIndex
     var value: T = 0
     var shift: UInt = 0
@@ -30,3 +30,69 @@ func decodeSLEB128<T>(_ bytes: ArraySlice<UInt8>, _: T.Type) -> (value: T, offse
     }
     return (value, index - bytes.startIndex)
 }
+
+func encodeULEB128<T>(_ value: T, padTo: Int? = nil) -> [UInt8]
+    where T: UnsignedInteger, T: FixedWidthInteger {
+    var value = value
+    var length = 0
+    var results: [UInt8] = []
+    var needPad: Bool {
+        guard let padTo = padTo else { return false }
+        return length < padTo
+    }
+    repeat {
+        var byte = UInt8(value & 0x7f)
+        value >>= 7
+        length += 1
+        if value != 0 || needPad {
+            byte |= 0x80
+        }
+        results.append(byte)
+    } while (value != 0)
+    
+    if let padTo = padTo, length < padTo {
+        while length < padTo - 1 {
+            results.append(0x80)
+            length += 1
+        }
+        results.append(0x00)
+    }
+    return results
+}
+
+
+func encodeSLEB128<T>(_ value: T, padTo: Int? = nil) -> [UInt8]
+    where T: SignedInteger, T: FixedWidthInteger {
+    var value = value
+    var length = 0
+    var results: [UInt8] = []
+    var hasMore: Bool
+    var needPad: Bool {
+        guard let padTo = padTo else { return false }
+        return length < padTo
+    }
+    repeat {
+        var byte = UInt8(value & 0x7f)
+        value >>= 7
+        length += 1
+        hasMore = !(
+            (value == 0  && (byte & 0x40) == 0) ||
+            (value == -1 && (byte & 0x40) != 0)
+        )
+        if hasMore || needPad {
+            byte |= 0x80
+        }
+        results.append(byte)
+    } while (hasMore)
+
+    if let padTo = padTo, length < padTo {
+        let padValue: UInt8 = value < 0 ? 0x7f : 0x00
+        while length < padTo - 1 {
+            results.append(padValue | 0x80)
+            length += 1
+        }
+        results.append(padValue)
+    }
+    return results
+}
+
--- Tests/ChibiLinkTests/LEB128Tests.swift
+++ Tests/ChibiLinkTests/LEB128Tests.swift
@@ -50,4 +50,64 @@ class LEB128Tests: XCTestCase {
         EXPECT_DECODE_SLEB128_EQ(64, [0xc0, 0x00])
         EXPECT_DECODE_SLEB128_EQ(-12345, [0xc7, 0x9f, 0x7f])
     }
+    
+    func testEncodeULEB128() {
+        
+        func EXPECT_ULEB128_EQ<T>(
+            _ expected: [UInt8], _ value: T, _ padTo: Int? = nil,
+            file: StaticString = #filePath, line: UInt = #line
+        ) where T: UnsignedInteger, T: FixedWidthInteger {
+            XCTAssertEqual(encodeULEB128(value, padTo: padTo), expected, file: file, line: line)
+        }
+        
+        EXPECT_ULEB128_EQ([0x00], 0 as UInt32)
+        EXPECT_ULEB128_EQ([0x01], 1 as UInt32)
+        EXPECT_ULEB128_EQ([0x3f], 63 as UInt32)
+        EXPECT_ULEB128_EQ([0x40], 64 as UInt32)
+        EXPECT_ULEB128_EQ([0x7f], 0x7f as UInt32)
+        EXPECT_ULEB128_EQ([0x80, 0x01], 0x80 as UInt32)
+        EXPECT_ULEB128_EQ([0x81, 0x01], 0x81 as UInt32)
+        EXPECT_ULEB128_EQ([0x90, 0x01], 0x90 as UInt32)
+        EXPECT_ULEB128_EQ([0xff, 0x01], 0xff as UInt32)
+        EXPECT_ULEB128_EQ([0x80, 0x02], 0x100 as UInt32)
+        EXPECT_ULEB128_EQ([0x81, 0x02], 0x101 as UInt32)
+
+        EXPECT_ULEB128_EQ([0x80, 0x00], 0 as UInt32, 2)
+        EXPECT_ULEB128_EQ([0x80, 0x80, 0x00], 0 as UInt32, 3)
+        EXPECT_ULEB128_EQ([0xff, 0x00], 0x7f as UInt32, 2)
+        EXPECT_ULEB128_EQ([0xff, 0x80, 0x00], 0x7f as UInt32, 3)
+        EXPECT_ULEB128_EQ([0x80, 0x81, 0x00], 0x80 as UInt32, 3)
+        EXPECT_ULEB128_EQ([0x80, 0x81, 0x80, 0x00], 0x80 as UInt32, 4)
+    }
+    
+    func testEncodeSLEB128() {
+        
+        func EXPECT_SLEB128_EQ<T>(
+            _ expected: [UInt8], _ value: T, _ padTo: Int? = nil,
+            file: StaticString = #filePath, line: UInt = #line
+        ) where T: SignedInteger, T: FixedWidthInteger {
+            XCTAssertEqual(encodeSLEB128(value, padTo: padTo), expected, file: file, line: line)
+        }
+
+        EXPECT_SLEB128_EQ([0x00], 0)
+        EXPECT_SLEB128_EQ([0x01], 1)
+        EXPECT_SLEB128_EQ([0x7f], -1)
+        EXPECT_SLEB128_EQ([0x3f], 63)
+        EXPECT_SLEB128_EQ([0x41], -63)
+        EXPECT_SLEB128_EQ([0x40], -64)
+        EXPECT_SLEB128_EQ([0xbf, 0x7f], -65)
+        EXPECT_SLEB128_EQ([0xc0, 0x00], 64)
+
+        EXPECT_SLEB128_EQ([0x80, 0x00], 0, 2)
+        EXPECT_SLEB128_EQ([0x80, 0x80, 0x00], 0, 3)
+        EXPECT_SLEB128_EQ([0xff, 0x80, 0x00], 0x7f, 3)
+        EXPECT_SLEB128_EQ([0xff, 0x80, 0x80, 0x00], 0x7f, 4)
+        EXPECT_SLEB128_EQ([0x80, 0x81, 0x00], 0x80, 3)
+        EXPECT_SLEB128_EQ([0x80, 0x81, 0x80, 0x00], 0x80, 4)
+        EXPECT_SLEB128_EQ([0xc0, 0x7f], -0x40, 2)
+
+        EXPECT_SLEB128_EQ([0xc0, 0xff, 0x7f], -0x40, 3)
+        EXPECT_SLEB128_EQ([0x80, 0xff, 0x7f], -0x80, 3)
+        EXPECT_SLEB128_EQ([0x80, 0xff, 0xff, 0x7f], -0x80, 4)
+    }
 }
