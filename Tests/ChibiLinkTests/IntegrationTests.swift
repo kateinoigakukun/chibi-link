ef001d537888d6a6db08360b0b05932a7220076f
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-04 18:10:26 +0900

    Add integration test

--- Tests/ChibiLinkTests/IntegrationTests.swift
+++ Tests/ChibiLinkTests/IntegrationTests.swift
@@ -1,8 +1,20 @@
-//
-//  IntegrationTests.swift
-//  ChibiLinkTests
-//
-//  Created by kateinoigakukun on 2020/09/04.
-//
+import ChibiLink
+import XCTest
 
-import Foundation
+class IntegrationTests: XCTestCase {
+    
+    func testLinkSwiftStdlib() throws {
+        let experiment = URL(fileURLWithPath: #file)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .appendingPathComponent("Experiment")
+        let inputs: [URL] = [
+            experiment.appendingPathComponent("shared_lib.wasm"),
+            experiment.appendingPathComponent("main.o"),
+        ]
+        let output = experiment.appendingPathComponent("linked.wasm")
+        try performLinker(inputs.map(\.path), output: output.path)
+    }
+}
