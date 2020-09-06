4d7f751a53c2e5c35a24df7a529e6aa6046aebaa
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-07 00:24:25 +0900

    Setup executable for performance tuning

--- Sources/chibi-link/main.swift
+++ Sources/chibi-link/main.swift
@@ -1,1 +1,16 @@
+import ChibiLink
+import Foundation
 
+let experiment = URL(fileURLWithPath: #file)
+    .deletingLastPathComponent()
+    .deletingLastPathComponent()
+    .deletingLastPathComponent()
+    .deletingLastPathComponent()
+    .appendingPathComponent("TokamakPad")
+    .appendingPathComponent("Experiment")
+let inputs: [URL] = [
+    experiment.appendingPathComponent("shared_lib.wasm"),
+    experiment.appendingPathComponent("main.o"),
+]
+let output = experiment.appendingPathComponent("linked.wasm")
+try performLinker(inputs.map(\.path), output: output.path)
